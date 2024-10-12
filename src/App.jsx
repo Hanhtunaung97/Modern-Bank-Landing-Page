bae35fd3396a984fb26913b785c934d65f66a506
Hanhtunaung97 <littlehanhtunaung97>
2024-10-12 15:39:45 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Nav } from './components'
+
+const App = () => {
+  return (
+    <main className='bg-primary'>
+      <Nav/>
+
+    </main>
+  )
+}
+
+export default App
